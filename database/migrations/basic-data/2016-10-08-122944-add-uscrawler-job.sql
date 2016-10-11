c251b147fadace399db039315422b58c68e61a1e
Radim Jílek <resan.xorel@gmail.com>
2016-10-11 15:13:54 +0200

    Fix insert USCrawler job to DB

--- database/migrations/basic-data/2016-10-08-122944-add-uscrawler-job.sql
+++ database/migrations/basic-data/2016-10-08-122944-add-uscrawler-job.sql
@@ -1,2 +1,2 @@
 -- Add data for US crawler
-INSERT INTO job (name, description, database_user_id) VALUES ('App\Commands\USCrawler', 'Constitutional Court Crawler', (SELECT currval('user_id_user_seq')));
+INSERT INTO job (name, description, database_user_id) VALUES ('App\Commands\USCrawler', 'Constitutional Court Crawler', 3);
