d1c4310313b613807402da2a5a81a0a5b86b21c4
Radim Jílek <resan.xorel@gmail.com>
2017-02-02 22:01:38 +0100

    Adding text to the web creating AboutPresenter

---
+++ app/Presenters/AboutPresenter.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Radim JĂ­lek
+ * Date: 02.02.2017
+ * Time: 20:35
+ */
+
+namespace App\Presenters;
+
+
+class AboutPresenter extends BasePresenter
+{
+    public function actionContact()
+    {
+    }
+
+    public function actionProject()
+    {
+    }
+
+}
