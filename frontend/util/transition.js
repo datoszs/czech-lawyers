43eb165e9b4319759e0ea6c94eb367eaf6377e6f
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-02-26 16:28:07 +0100

    #68 util function for route change

---
+++ frontend/util/transition.js
@@ -0,0 +1,16 @@
+import {browserHistory, formatPattern} from 'react-router';
+import querystring from 'query-string';
+
+/**
+ * Transitions application to a new route (page).
+ * @param module Route module (must contain ROUTE constant).
+ * @param params Route parameters (optional).
+ * @param query Route query (optional).
+ */
+export default (module, params, query) => {
+    let route = formatPattern(module.ROUTE, params);
+    if (query) {
+        route += `?${querystring.stringify(query)}`;
+    }
+    browserHistory.push(route);
+};
