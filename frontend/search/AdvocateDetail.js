3cd8d518acd4ea78bc6dd19c8748b94f51ba086a
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-03-03 23:35:44 +0100

    #75 another attempt

--- frontend/search/AdvocateDetail.js
+++ frontend/search/AdvocateDetail.js
@@ -1,5 +1,5 @@
 import React, {PropTypes} from 'react';
-import {Panel, Button, Label, Row, Col} from 'react-bootstrap';
+import {Panel, Button, Label, Row, Col, Glyphicon} from 'react-bootstrap';
 import Statistics from './Statistics';
 
 const AdvocateDetail = ({name, IC, city, status, positive, negative, neutral}) => {
@@ -14,27 +14,29 @@ const AdvocateDetail = ({name, IC, city, status, positive, negative, neutral}) =
         removed: 'Vyškrtnutý',
     }[status];
 
+    const footer = (
+        <Row>
+            <Col md={3}><h5><b>Sídlo</b></h5>{city}</Col>
+            <Col md={3}><h5><b>IČ</b></h5>{IC}</Col>
+            <Col md={3}><h5><b>Status</b></h5><Label bsStyle={bsStyle}>{label}</Label></Col>
+            <Col md={2}><Button>Více &gt;</Button></Col>
+        </Row>
+    );
+
     return (
-        <Panel bsStyle={bsStyle} style={{cursor: 'pointer'}}>
+        <Panel bsStyle={bsStyle} style={{cursor: 'pointer'}} footer={footer}>
             <Row>
-                <Col md={9}>
+                <Col md={8}>
                     <h2 style={{display: 'inline-block'}}>{name}</h2>
-
                 </Col>
-
-                <Col md={3}>
+                <Col md={4}>
                     <Statistics number={positive} scale={1.2} color="green" />
                     <Statistics number={negative} scale={0.8} color="red" />
                     <Statistics number={neutral} scale={0.5} color="gray" />
+                    <Glyphicon glyph="question-sign" />
                 </Col>
-            </Row>
-            <Row>
-                <Col md={3}>IČ: {IC}</Col>
-                <Col md={3}>{city}</Col>
-                <Col md={4} />
-                <Col md={3}>
-                    <Label bsStyle={bsStyle}>{label}</Label>
-                </Col>
+
+
             </Row>
         </Panel>
     );
