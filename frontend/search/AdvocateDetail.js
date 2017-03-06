119e4f08ab8d9d03eda57f44565c88e1ca92f863
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-03-06 09:17:24 +0100

    #75 additional styling

--- frontend/search/AdvocateDetail.js
+++ frontend/search/AdvocateDetail.js
@@ -1,15 +1,10 @@
 import React, {PropTypes} from 'react';
-import {Panel, Label, Row, Col} from 'react-bootstrap';
+import {Panel, Row, Col} from 'react-bootstrap';
 import Statistics from './Statistics';
 
-const normalizeScale = (scale) => Math.min(Math.max(1, scale), 1.6);
+const normalizeScale = (scale) => Math.min(Math.max(1, scale), 1.5);
 
 const AdvocateDetail = ({name, IC, city, status, positive, negative, neutral}) => {
-    const bsStyle = {
-        active: 'success',
-        suspended: 'warning',
-        removed: 'danger',
-    }[status];
     const label = {
         active: 'Aktivní',
         suspended: 'Pozastavený',
@@ -20,20 +15,19 @@ const AdvocateDetail = ({name, IC, city, status, positive, negative, neutral}) =
         <Row>
             <Col md={4}><b>{city}</b></Col>
             <Col md={4}>IČ <b>{IC}</b></Col>
+            <Col md={4}><b>{label}</b></Col>
         </Row>
     );
 
     const average = (positive + negative + neutral) / 3;
 
     return (
-        <Panel bsStyle={bsStyle} style={{cursor: 'pointer'}} footer={footer}>
+        <Panel style={{cursor: 'pointer'}} footer={footer}>
             <Row>
                 <Col md={8}>
                     <h2>{name}</h2>
-                    <h4><Label bsStyle={bsStyle}>{label}</Label></h4>
                 </Col>
-                <Col md={4}>
-                    <br /><br />
+                <Col md={4} style={{marginTop: 15}}>
                     <Statistics number={positive} scale={normalizeScale(positive / average)} color="green" />
                     <Statistics number={negative} scale={normalizeScale(negative / average)} color="red" />
                     <Statistics number={neutral} scale={normalizeScale(neutral / average)} color="gray" />
