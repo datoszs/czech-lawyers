9537981b1d0fcedf41840211f667c36e5cf26299
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-03-15 21:10:19 +0100

    #82 advocate detail plugged in

---
+++ frontend/advocate/index.js
@@ -0,0 +1,12 @@
+import {NAME, ROUTE} from './constants';
+import saga from './saga';
+import reducer from './reducer';
+import Container from './Container';
+
+export default {
+    NAME,
+    ROUTE,
+    saga,
+    reducer,
+    Container,
+};
--- frontend/saga.js
+++ frontend/saga.js
@@ -3,10 +3,12 @@ import {call} from 'redux-saga/effects';
 import router from './router';
 import search from './search';
 import home from './home';
+import advocate from './advocate';
 
 export default function* () {
     yield call(router.saga, [
         search,
         home,
+        advocate,
     ]);
 }
