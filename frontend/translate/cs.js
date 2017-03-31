2317472138ea92e7e87449da0f3e4c8232ee27d1
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-03-31 13:10:47 +0200

    #92 copyright corrected

--- frontend/translate/cs.js
+++ frontend/translate/cs.js
@@ -4,7 +4,7 @@ export default {
     'about.title': 'O projektu',
     'contact.title': 'Kontakt',
     'contact.appeal': 'Napište nám',
-    'copyright': '\u{A9} 2017 DATAOS \u{2014} data o spravedlnosti, z.s',
+    'copyright': '\u{A9} 2017 DATOS \u{2014} data o spravedlnosti, z.s',
 
     'search.button': 'Hledat',
     'search.placeholder': 'Zadejte jméno nebo IČ',
