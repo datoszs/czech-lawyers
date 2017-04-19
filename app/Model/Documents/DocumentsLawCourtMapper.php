4a2a2f8d870d549a276ef12c619bb433d8dd844d
Radim Jílek <resan.xorel@gmail.com>
2017-04-19 22:06:12 +0200

    Add support for JSONB on NSS, US documents

--- app/Model/Documents/DocumentsLawCourtMapper.php
+++ app/Model/Documents/DocumentsLawCourtMapper.php
@@ -1,10 +1,27 @@
 <?php
 namespace App\Model\Documents;
 
+use Mikulas\OrmExt\MappingFactory;
 use Nextras\Orm\Mapper\Mapper;
 
 class DocumentsLawCourtMapper extends Mapper
 {
+	protected function createStorageReflection()
+	{
+		$factory = new MappingFactory(parent::createStorageReflection(), $this->getRepository()->getEntityMetadata());
+		$factory->addJsonMapping('proposer');
+		$factory->addJsonMapping('institutionConcerned');
+		$factory->addJsonMapping('contestedAct');
+		$factory->addJsonMapping('concernedLaws');
+		$factory->addJsonMapping('concernedOther');
+		$factory->addJsonMapping('dissentingOpinion');
+		$factory->addJsonMapping('proceedingsSubject');
+		$factory->addJsonMapping('subjectIndex');
+		$factory->addJsonMapping('names');
+
+		return $factory->getStorageReflection();
+	}
+
 	public function getTableName()
 	{
 		return 'document_law_court';
--- app/Model/Documents/DocumentsSupremeAdministrativeCourtMapper.php
+++ app/Model/Documents/DocumentsSupremeAdministrativeCourtMapper.php
@@ -1,10 +1,20 @@
 <?php
 namespace App\Model\Documents;
 
+use Mikulas\OrmExt\MappingFactory;
 use Nextras\Orm\Mapper\Mapper;
 
 class DocumentsSupremeAdministrativeCourtMapper extends Mapper
 {
+	protected function createStorageReflection()
+	{
+		$factory = new MappingFactory(parent::createStorageReflection(), $this->getRepository()->getEntityMetadata());
+		$factory->addJsonMapping('sides');
+		$factory->addJsonMapping('prejudicate');
+
+		return $factory->getStorageReflection();
+	}
+
 	public function getTableName()
 	{
 		return 'document_supreme_administrative_court';
