78127216309a0386a16af1d3d27154f9473732b2
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-04-22 15:57:58 +0200

    fixes #118 panel removed from "Documents" heading

--- frontend/case/DocumentContainer.js
+++ frontend/case/DocumentContainer.js
@@ -1,14 +1,13 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {List} from 'immutable';
-import {Panel} from 'react-bootstrap';
 import {Msg} from '../containers';
 import {getDocuments} from './selectors';
 import DocumentDetail from './DocumentDetail';
 
 const DocumentContainer = ({documents}) => (
     <section>
-        <h1><Panel><Msg msg="case.documents" /></Panel></h1>
+        <header><h1><Msg msg="case.documents" /></h1></header>
         {documents.map((document) => <DocumentDetail key={document} id={document} />)}
     </section>
 );
