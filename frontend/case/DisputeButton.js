80f07a8dca4356a8a1cc0d5fbc501c3f25f7bc7b
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-05-19 14:00:24 +0200

    #129 dispute final conditions

--- frontend/case/DisputeButton.js
+++ frontend/case/DisputeButton.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {Button} from 'react-bootstrap';
-import {Msg} from '../containers';
+import {Button, Panel} from 'react-bootstrap';
+import {Msg, RichText} from '../containers';
 import DisputeForm from './DisputeForm';
-import {isDisputed, isDisputeFormOpen} from './selectors';
+import {isDisputed, isDisputeFormOpen, getDetail} from './selectors';
 import {openDisputeForm} from './actions';
 
-const DisputeButtonComponent = ({formOpen, disputed, openForm}) => {
-    if (formOpen) {
+const DisputeButtonComponent = ({formOpen, disputed, openForm, final}) => {
+    if (final) {
+        return <Panel bsStyle="danger"><RichText msg="case.dispute.final.both" /></Panel>;
+    } else if (formOpen) {
         return <DisputeForm />;
     } else {
         return <Button bsStyle="danger" disabled={disputed} onClick={openForm}><Msg msg="case.dispute" /></Button>;
@@ -19,12 +21,17 @@ DisputeButtonComponent.propTypes = {
     formOpen: PropTypes.bool.isRequired,
     disputed: PropTypes.bool.isRequired,
     openForm: PropTypes.func.isRequired,
+    final: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-    formOpen: isDisputeFormOpen(state),
-    disputed: isDisputed(state),
-});
+const mapStateToProps = (state) => {
+    const detail = getDetail(state);
+    return {
+        formOpen: isDisputeFormOpen(state),
+        disputed: isDisputed(state),
+        final: !!detail && detail.advocateFinal && detail.resultFinal,
+    };
+};
 
 const mapDispatchToProps = (dispatch) => ({
     openForm: () => dispatch(openDisputeForm()),
