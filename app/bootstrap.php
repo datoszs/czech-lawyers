d5c6167517f5a7ef15da614f0d0cdd6ea4e220de
Jan Drábek <jan.drabek@trigama.eu>
2017-07-02 21:45:45 +0200

    Fix error response code on deployment when using ajax

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,6 +7,8 @@ $configurator = new Nette\Configurator;
 if (PHP_SAPI !== 'cli' && (file_exists(__DIR__ . '/../.deployment-in-progress') || file_exists(__DIR__ . '/../.deployment'))) {
 	// AJAX requests needs special treatment
 	if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
+		header('HTTP/1.1 503 Service Unavailable');
+		header('Retry-After: 900'); // 15 minutes in seconds
 		echo json_encode(['deployment' => TRUE]);
 		exit;
 	} else {
