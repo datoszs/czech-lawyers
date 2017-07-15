7f1b8f1984830206ff9052e1afa46f190fd09db7
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-07-15 15:29:14 +0200

    #114 dates in case detail

--- frontend/case/Detail.js
+++ frontend/case/Detail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import moment from 'moment';
 import {courtsMsg, resultMsg} from '../model';
 import {DetailField} from '../containers';
 import {wrapEventStop} from '../util';
@@ -10,11 +11,13 @@ import {ADVOCATE_DETAIL} from '../routes';
 import {getDetail} from './selectors';
 
 
-const DetailComponent = ({advocateName, court, result, handleAdvocate}) => (
+const DetailComponent = ({advocateName, court, result, decisionDate, propositionDate, handleAdvocate}) => (
     <div>
         <DetailField msg="case.advocate"><a href="" onClick={wrapEventStop(handleAdvocate)} >{advocateName}</a></DetailField>
         <DetailField msg="case.court">{court}</DetailField>
         <DetailField msg="case.result">{result}</DetailField>
+        <DetailField msg="case.date.proposition">{propositionDate}</DetailField>
+        <DetailField msg="case.date.decision">{decisionDate}</DetailField>
     </div>
 );
 
@@ -22,6 +25,8 @@ DetailComponent.propTypes = {
     advocateName: PropTypes.string,
     court: PropTypes.string,
     result: PropTypes.string,
+    decisionDate: PropTypes.string,
+    propositionDate: PropTypes.string,
     handleAdvocate: PropTypes.func.isRequired,
 };
 
@@ -29,15 +34,20 @@ DetailComponent.defaultProps = {
     advocateName: null,
     court: null,
     result: null,
+    decisionDate: null,
+    propositionDate: null,
 };
 
 const mapStateToProps = (state) => {
     const caseDetail = getDetail(state);
+    const dateFormat = translate.getShortDateFormat(state);
     return ({
         advocateName: caseDetail && caseDetail.advocateName,
         court: caseDetail && translate.getMessage(state, courtsMsg[caseDetail.court]),
         result: caseDetail && translate.getMessage(state, resultMsg[caseDetail.result]),
         advocateId: caseDetail && caseDetail.advocateId,
+        decisionDate: caseDetail && caseDetail.decisionDate && moment(caseDetail.decisionDate).format(dateFormat),
+        propositionDate: caseDetail && caseDetail.propositionDate && moment(caseDetail.propositionDate).format(dateFormat),
     });
 };
 
