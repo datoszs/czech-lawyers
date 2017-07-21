5f17dc8757cb412e5a01969efed0ee20b05daa45
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-07-21 14:14:45 +0200

    #122 fixed scroll on advocate change
    when the user would switch advocates (e.g. due to same-name advocates), the page would scroll to cases

--- frontend/advocate/CaseScroller.js
+++ frontend/advocate/CaseScroller.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Element, scroller} from 'react-scroll';
-import {getCases} from './selectors';
+import {getCases, getAdvocate} from './selectors';
 
 class CaseScrollerComponent extends Component {
     constructor(props) {
@@ -11,7 +11,8 @@ class CaseScrollerComponent extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        if (this.props.name === nextProps.name && this.empty && !nextProps.empty) {
+        const isSame = (prop) => (this.props[prop] === nextProps[prop]);
+        if (isSame('name') && isSame('advocate') && this.empty && !nextProps.empty) {
             setTimeout(() => {
                 scroller.scrollTo(this.props.name);
             });
@@ -31,10 +32,12 @@ class CaseScrollerComponent extends Component {
 CaseScrollerComponent.propTypes = {
     empty: PropTypes.bool.isRequired,
     name: PropTypes.string.isRequired,
+    advocate: PropTypes.number.isRequired, // eslint-disable-line react/no-unused-prop-types, linter error
 };
 
 const mapStateToProps = (state) => ({
     empty: getCases(state).size === 0,
+    advocate: getAdvocate(state),
 });
 
 const CaseScroller = connect(mapStateToProps)(CaseScrollerComponent);
