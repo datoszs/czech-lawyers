8e299ea4e0c5bc156eba2234dee62811e1fbc26e
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-10-06 15:06:57 +0200

    #198 events for advocate and case inspection

--- frontend/case/saga.js
+++ frontend/case/saga.js
@@ -1,4 +1,5 @@
 import {put, call, select, takeEvery} from 'redux-saga/effects';
+import ga from 'react-ga';
 import {caseAPI} from '../serverAPI';
 import {mapDtoToCaseDetail} from '../model';
 import formstatus from '../formstatus';
@@ -24,6 +25,12 @@ export default function* caseRoute({id}) {
     if (!(yield select(isDetailLoaded))) {
         const caseDto = yield call(caseAPI.get, id);
         yield put(setDetail(mapDtoToCaseDetail(caseDto)));
+
+        ga.event({
+            category: 'case',
+            action: 'inspect',
+            label: caseDto.registry_mark,
+        });
     }
 
     yield takeEvery(DISPUTE, sendDisputeSaga, id);
