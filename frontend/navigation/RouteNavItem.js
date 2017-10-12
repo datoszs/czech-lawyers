34ae9570b88d17341655527e795293ae1778f2db
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-10-12 22:27:49 +0200

    #184 links for navitems

--- frontend/navigation/RouteNavItem.js
+++ frontend/navigation/RouteNavItem.js
@@ -1,20 +1,23 @@
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {NavItem} from 'react-bootstrap';
+import {wrapLinkMouseEvent} from '../util';
 import router from '../router';
 
 const mapStateToProps = (state, {module}) => ({
     active: router.isActive(state, module),
+    href: router.getCurrentHref(state, module),
 });
 
 const mapDispatchToProps = (dispatch, {module}) => ({
-    onClick: () => dispatch(router.navigate(module)),
+    onClick: wrapLinkMouseEvent(() => dispatch(router.navigate(module))),
 });
 
-const mergeProps = ({active}, {onClick}, {children}) => ({
+const mergeProps = ({active, href}, {onClick}, {children}) => ({
     active,
     onClick,
     children,
+    href,
 });
 
 const RouteNavItem = connect(mapStateToProps, mapDispatchToProps, mergeProps)(NavItem);
