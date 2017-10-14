7dcf3a5d75a2d0936372a56f3831733b5f5cd18d
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-10-14 20:11:36 +0200

    #155 timeline

--- frontend/components/timeline/Bar.js
+++ frontend/components/timeline/Bar.js
@@ -2,13 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {wrapEventStop} from '../../util';
+import styles from './Bar.css';
+
+const styleMap = {
+    positive: styles.positive,
+    negative: styles.negative,
+    neutral: styles.neutral,
+};
 
 const Bar = ({size, className, selected, onClick}) => (
     <div
         className={classnames({
-            [className]: true,
-            'timeline-bar': true,
-            selected,
+            [styleMap[className]]: true,
+            [styles.selected]: selected,
         })}
         style={{height: `${size * 100}%`}}
         onClick={wrapEventStop(onClick)}
