9d1d74aeb78e531972434aacec2caca9a398638b
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-10-20 11:01:38 +0200

    #125 leaderboard column graphics

--- frontend/home/Container.js
+++ frontend/home/Container.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Jumbotron, Row, Col, Alert} from 'react-bootstrap';
+import {Jumbotron, Row, Alert} from 'react-bootstrap';
 import {Msg, RichText} from '../containers';
 import {ApplicationTitle} from '../components';
 import autocomplete from '../autocomplete';
-import {getTop, getBottom} from './selectors';
-import LeaderBoard from './LeaderBoard';
+import {courts} from '../model';
+import LeaderBoardColumn from './LeaderBoardColumn';
 import ProblemStatements from './ProblemStatements';
 
 const Container = () => (
@@ -20,9 +20,9 @@ const Container = () => (
         <ProblemStatements />
         <RichText msg="leaderboard.legend" />
         <Row>
-            <Col sm={0} lg={2} />
-            <Col sm={6} lg={4}><LeaderBoard type="positive" selector={getTop} /></Col>
-            <Col sm={6} lg={4}><LeaderBoard type="negative" selector={getBottom} /></Col>
+            <LeaderBoardColumn court={courts.NS} />
+            <LeaderBoardColumn court={courts.NSS} />
+            <LeaderBoardColumn court={courts.US} />
         </Row>
         <Alert bsStyle="warning"><Msg msg="home.disclaimer" /></Alert>
     </section>
---
+++ frontend/home/LeaderBoardColumn.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Col, Panel} from 'react-bootstrap';
+import {Msg} from '../containers';
+import {courts, courtsMsg} from '../model';
+import LeaderBoard from './LeaderBoard';
+import {getTop, getBottom} from './selectors';
+
+const LeaderBoardColumn = ({court}) => (
+    <Col lg={4}>
+        <Panel header={<p><Msg msg={courtsMsg[court]} /></p>} />
+        <LeaderBoard court={court} selector={getTop} type="positive" />
+        <LeaderBoard court={court} selector={getBottom} type="negative" />
+    </Col>
+);
+
+LeaderBoardColumn.propTypes = {
+    court: PropTypes.oneOf(Object.values(courts)).isRequired,
+};
+
+export default LeaderBoardColumn;
