7747083441cc5733826ecd0c140955699d498db3
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-10-20 15:29:54 +0200

    #125 court-specific leaderboards

--- frontend/home/reducer.js
+++ frontend/home/reducer.js
@@ -5,14 +5,17 @@ import {SET_LEADERBOARD} from './actions';
 const getId = (advocate) => advocate.id_advocate;
 const compare = (advocateA, advocateB) => advocateA.sorting_name.localeCompare(advocateB.sorting_name);
 const getIdList = (advocates) => List(advocates.sort(compare).map(getId));
-const topReducer = (state = List(), action) => (action.type === SET_LEADERBOARD ? getIdList(action.top) : state);
-const bottomReducer = (state = List(), action) => (action.type === SET_LEADERBOARD ? getIdList(action.bottom) : state);
+const getCourtMap = (courtMap) => new Map(Object.entries(courtMap).map(([court, advocates]) => [parseInt(court, 10), getIdList(advocates)]));
+const courtMapReducer = (property) => (state = Map(), action) => (action.type === SET_LEADERBOARD ? getCourtMap(action[property]) : state);
 
-const getMap = (...arrays) => Map([].concat(...arrays.map((array) => array.map((advocate) => [advocate.id_advocate, advocate.fullname]))));
-const nameReducer = (state = Map(), action) => (action.type === SET_LEADERBOARD ? getMap(action.top, action.bottom) : state);
+const getAdvocateLists = (courtMap) =>
+    Object.values(courtMap).map((advocates) => advocates.map((advocate) => [advocate.id_advocate, advocate.fullname]));
+const getMap = (...arrays) => Map([].concat(...arrays));
+const nameReducer = (state = Map(), action) =>
+    (action.type === SET_LEADERBOARD ? getMap(...getAdvocateLists(action.top), ...getAdvocateLists(action.bottom)) : state);
 
 export default combineReducers({
-    top: topReducer,
-    bottom: bottomReducer,
+    top: courtMapReducer('top'),
+    bottom: courtMapReducer('bottom'),
     names: nameReducer,
 });
--- frontend/home/selectors.js
+++ frontend/home/selectors.js
@@ -1,7 +1,8 @@
+import {List} from 'immutable';
 import {NAME} from './constants';
 
 const getModel = (state) => state.get(NAME);
 
-export const getTop = (state, court) => getModel(state).get('top');
-export const getBottom = (state, court) => getModel(state).get('bottom');
+export const getTop = (state, court) => getModel(state).getIn(['top', court], List());
+export const getBottom = (state, court) => getModel(state).getIn(['bottom', court], List());
 export const getName = (state, id) => getModel(state).getIn(['names', id]);
