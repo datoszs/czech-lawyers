be618ac0a624519858e9676ceb0740d5644dfcf0
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-12-15 12:29:18 +0100

    #217 missing document displayed on case detail

--- frontend/case/DocumentDetail.js
+++ frontend/case/DocumentDetail.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {Glyphicon, Row, Col} from 'react-bootstrap';
 import moment from 'moment';
 import translate from '../translate';
 import {DetailPanel} from '../components';
+import {Msg} from '../containers';
 import {getDocument} from './selectors';
 
 const mapStateToProps = (state, {id}) => {
@@ -19,12 +22,20 @@ const mapDispatchToProps = () => ({
     openDetail: (href) => () => window.open(href),
 });
 
-const mergeProps = ({mark, date, href, dateFormat}, {openDetail}) => ({
-    title: mark,
-    footer: moment(date).format(dateFormat),
-    onClick: openDetail(href),
-    href,
-});
+const mergeProps = ({mark, date, href, dateFormat}, {openDetail}) => {
+    const formattedDate = moment(date).format(dateFormat);
+    return ({
+        title: mark,
+        footer: href ? formattedDate : (
+            <Row>
+                <Col md={3}>{formattedDate}</Col>
+                <Col md={9}><Glyphicon glyph="alert" /> <Msg msg="case.document.missing" /></Col>
+            </Row>
+        ),
+        onClick: href && openDetail(href),
+        href,
+    });
+};
 
 const DocumentDetail = connect(mapStateToProps, mapDispatchToProps, mergeProps)(DetailPanel);
 
