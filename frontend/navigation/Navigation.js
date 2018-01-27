4aa859e50d90da50f9787a7b914ccaba703b5d0a
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2018-01-27 11:09:12 +0100

    NSS error alert removed

--- frontend/navigation/Navigation.js
+++ frontend/navigation/Navigation.js
@@ -1,33 +1,29 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 import RouteNavItem from './RouteNavItem';
-import NssErrorAlert from './NssErrorAlert';
 
 import {Msg, RouterLink} from '../containers';
 
 import {HOME, ABOUT, CONTACT, ADVOCATE_SEARCH, CASE_SEARCH, EXPORT} from '../routes';
 
 const Navigation = () => (
-    <Fragment>
-        <NssErrorAlert />
-        <Navbar>
-            <Navbar.Header>
-                <Navbar.Brand><RouterLink route={HOME}><Msg msg="nav.title" /></RouterLink></Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav>
-                    <RouteNavItem module={ADVOCATE_SEARCH}><Msg msg="advocates.title" /></RouteNavItem>
-                    <RouteNavItem module={CASE_SEARCH}><Msg msg="cases.title" /></RouteNavItem>
-                </Nav>
-                <Nav pullRight>
-                    <RouteNavItem module={ABOUT}><Msg msg="about.title" /></RouteNavItem>
-                    <RouteNavItem module={EXPORT}><Msg msg="export.nav" /></RouteNavItem>
-                    <RouteNavItem module={CONTACT}><Msg msg="contact.title" /></RouteNavItem>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </Fragment>
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand><RouterLink route={HOME}><Msg msg="nav.title" /></RouterLink></Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav>
+                <RouteNavItem module={ADVOCATE_SEARCH}><Msg msg="advocates.title" /></RouteNavItem>
+                <RouteNavItem module={CASE_SEARCH}><Msg msg="cases.title" /></RouteNavItem>
+            </Nav>
+            <Nav pullRight>
+                <RouteNavItem module={ABOUT}><Msg msg="about.title" /></RouteNavItem>
+                <RouteNavItem module={EXPORT}><Msg msg="export.nav" /></RouteNavItem>
+                <RouteNavItem module={CONTACT}><Msg msg="contact.title" /></RouteNavItem>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 );
 
 export default Navigation;
