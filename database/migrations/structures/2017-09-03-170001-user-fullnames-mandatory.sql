f957ba7bafd78c61f4a1670bc4aa2b65d5368f3a
Jan Drábek <jan@drabek.cz>
2020-04-03 11:08:53 +0200

    Fix migrations to succeed from clean state

--- database/migrations/structures/2017-09-03-170001-user-fullnames-mandatory.sql
+++ database/migrations/structures/2017-09-03-170001-user-fullnames-mandatory.sql
@@ -1,1 +1,5 @@
+UPDATE "user" SET fullname = 'System Import' where id_user = 1;
+UPDATE "user" SET fullname = 'System Tagging' where id_user = 2;
+UPDATE "user" SET fullname = 'System Crawler' where id_user = 3;
+
 ALTER TABLE "user" ALTER COLUMN fullname SET NOT NULL;
